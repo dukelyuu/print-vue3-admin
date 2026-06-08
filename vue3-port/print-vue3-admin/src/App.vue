0b3c9922da4634c3ea291ede982014f6191dd2d7
dukelyu <duke.lv@hotmail.com>
2026-06-08 15:01:32 +0800

    feat(vue3): add vue3-port/print-vue3-admin/src/App.vue

---
+++ vue3-port/print-vue3-admin/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <router-view />
+</template>
+
+<script setup>
+// Vue 3 SFC 写法，无需 export default
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB',
+    'Microsoft YaHei', SimSun, sans-serif;
+  font-size: 14px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+#app {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+}
+</style>
